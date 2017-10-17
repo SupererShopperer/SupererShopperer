f8b162a2fea9fdcb98620bf73a02c8a66d0e001a
Jo Zhou <jyz@MacBook-Pro-3.local>
2017-10-16 17:35:35 -0700

    added oauth button

--- src/components/Header.js
+++ src/components/Header.js
@@ -24,6 +24,7 @@ class Header extends Component {
         <Link to='/'>
           <img style={imgStyle} src={homeLogo} />
           </Link>
+        <a href="https://github.com/login/oauth/authorize?scope=user:email&amp;client_id=174cd191ab6c866f3007"><button className="oAuth">BUTTON</button></a>
       </div>
     );
   }
